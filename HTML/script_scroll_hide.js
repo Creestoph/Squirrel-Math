f4e12b85a2e676a0256b9e61a19a24782a84b1b2
KrzysztofStrzelecki <krzysztof.strzelecki@dentandbuckle.com>
2016-10-11 18:18:44 +0200

    block click while animating

--- HTML/script_scroll_hide.js
+++ HTML/script_scroll_hide.js
@@ -1,40 +1,44 @@
+var animate = 1;
 $(document).ready(function(){
 	$(".chapter_name").on("click", function(){
-		var dmask = $(this).parent().find(".chapter_mask");
-        var d = $(this).parent().find(".chapter_body");
-		var classList = dmask.attr('class').split(' ');
+		if (animate == 1)
+		{
+			var dmask = $(this).parent().find(".chapter_mask");
+			var d = $(this).parent().find(".chapter_body");
+			var classList = dmask.attr('class').split(' ');
+			
+			animate = 0;
+			if (d.hasClass("hidden"))
+			{
+				d.addClass("not_hidden");
+				d.removeClass("hidden");
+				
+				for (var i = 0; i < classList.length; i++)
+				{
+					if (classList[i].includes("height")) var dh = parseInt(classList[i].replace("height",""));	
+				}
+				d.animate({top: "+=" + dh}, 1100, "swing", function () {animate = 1;});
+				var dr = $(this).parent().find(".chapter_mask");
+				dr.animate({height: "+=" + dh}, 1100, "swing", function () {animate = 1;});
+				
+			}
+			else
+			{
+				d.addClass("hidden");
+				d.removeClass("not_hidden");
+				var toadd = true;
+				for (var i = 0; i < classList.length; i++)
+				{
+					if (classList[i].includes("height")) toadd = false;
+				}
+				if (toadd) dmask.addClass("height" + dmask.height());
+				
+				var dh =  -dmask.height();
+				d.animate({top: "+=" + dh}, 1100, "swing", function () {animate = 1;});
+				var dr = $(this).parent().find(".chapter_mask");
+				dr.animate({height: "+=" + dh}, 1100, "swing", function () {animate = 1;});
 		
-		if (d.hasClass("hidden"))
-		{
-			d.addClass("not_hidden");
-			d.removeClass("hidden");
-			
-			for (var i = 0; i < classList.length; i++)
-			{
-				if (classList[i].includes("height")) var dh = parseInt(classList[i].replace("height",""));	
 			}
-			d.animate({top: "+=" + dh, easing: "linear"}, 1100);
-            var dr = $(this).parent().find(".chapter_mask");
-			dr.animate({height: "+=" + dh, easing: "linear"}, 1100);
-			
-			
-		}
-		else
-		{
-			d.addClass("hidden");
-			d.removeClass("not_hidden");
-			var toadd = true;
-			for (var i = 0; i < classList.length; i++)
-			{
-				if (classList[i].includes("height")) toadd = false;
-			}
-			if (toadd) dmask.addClass("height" + dmask.height());
-			
-			var dh =  -dmask.height();
-			d.animate({top: "+=" + dh, easing: "linear"}, 1100);
-            var dr = $(this).parent().find(".chapter_mask");
-			dr.animate({height: "+=" + dh, easing: "linear"}, 1100);
-	
-		}
-	});        
+		}	
+	});  
 });
