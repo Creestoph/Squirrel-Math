30e8acba52c6def002e5afeb17abd62509e703df
Creestoph <krzstrzelecki@hotmail.com>
2016-11-28 19:51:28 +0100

    columnar_operation

--- Webpage/.scripts/sm_library.js
+++ Webpage/.scripts/sm_library.js
@@ -5,6 +5,6 @@
 divide = function (x,y) {
     if (parseInt(x) == 0) return 0;
     var i = 0;
-    while (parseInt(i*y) < parseInt(x)) i++;
+    while (parseInt(i*y) <= parseInt(x)) i++;
     return i-1;
 }
