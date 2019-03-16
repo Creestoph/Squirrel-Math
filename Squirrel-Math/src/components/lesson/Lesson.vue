344c799dbeff48311754adccfe746e9f923891cc
Creestoph <krzstrzelecki@hotmail.com>
2019-03-16 16:37:19 +0100

    test Lesson

--- Squirrel-Math/src/components/lesson/Lesson.vue
+++ Squirrel-Math/src/components/lesson/Lesson.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="row">
     <div class="col">
-      <lesson-title></lesson-title>
-      <lesson-chapter
-        v-for="(chapter, index) in chapters"
-        :key="index"
-        v-model="chapter">
-      </lesson-chapter>
+      <lesson-title>Title</lesson-title>
+      <lesson-intro>intro</lesson-intro>
+      <!--<lesson-chapter-->
+        <!--v-for="(chapter, index) in chapters"-->
+        <!--:key="index"-->
+        <!--v-model="chapter">-->
+      <!--</lesson-chapter>-->
+      <lesson-chapter title = "dhwaukdwa" body="dwjaukdhkuwad"></lesson-chapter>
     </div>
   </div>
 
@@ -15,20 +17,13 @@
 <script>
   import LessonTitle from './LessonTitle'
   import LessonIntro from './LessonIntro'
-  import LessonChapter from './LessonChapter'
+  import LessonChapter from './chapter/LessonChapter'
 
   export default {
     name: "Lesson",
     data() {
       return {
         chapters: [],
-        robert: 'a',
-        ks: [1,2,3,4,5]
-      }
-    },
-    methods: {
-      robertize(){
-        this.robert = 'roberto'
       }
     },
     components:{
