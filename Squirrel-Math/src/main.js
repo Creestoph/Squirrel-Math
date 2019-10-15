76287786573b50eead082a49131f87b48f1654df
Creestoph <creestoph.96@gmail.com>
2019-10-15 12:00:33 +0200

    #55 - Nawigacja między lekcjami poprzez router-link zachowuje pozycję scrolla przeglądarki

--- Squirrel-Math/src/main.js
+++ Squirrel-Math/src/main.js
@@ -9,7 +9,20 @@ import Comment from "./components/utils/Comment"
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
-const router = new VueRouter({routes});
+const router = new VueRouter({
+  routes,
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+    if (to.hash) {
+      return window.scrollTo({ 
+        top: document.querySelector(to.hash).offsetTop, 
+        behavior: 'smooth' 
+      })
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+});
 
 Vue.component('Comment', Comment);
 /* eslint-disable no-new */
