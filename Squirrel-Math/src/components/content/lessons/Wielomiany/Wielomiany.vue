687bf13d887f0e62f704ae3ffbabd5d2adbfd20c
Robert Salata <boberrs@gmail.com>
2019-12-20 22:49:07 +0100

    Wielomiany++

--- Squirrel-Math/src/components/content/lessons/Wielomiany/Wielomiany.vue
+++ Squirrel-Math/src/components/content/lessons/Wielomiany/Wielomiany.vue
@@ -266,7 +266,7 @@
     $$
 
     <p> 
-        Wystarczy rozwiązać pozostałe równanie kwadratowe (np. za pomocą wzoru na deltę). $x^2 - 11x + 28 = 0$ daje dwa rozwiązania $x = 4$ oraz $x = 7$, co dokańcza 
+        Wystarczy rozwiązać pozostałe równanie kwadratowe $x^2 - 11x + 28 = 0$ (np. za pomocą wzoru na deltę). Daje ono dwa rozwiązania $x = 4$ oraz $x = 7$, co dokańcza 
         rozkład na czynniki: $(x - 2)(x - 4)(x - 7)$ a zarazem kompletuje zbiór rozwiązań oryginalnego równania: $\{2, 4, 7\}$.
     </p>
 
@@ -353,6 +353,236 @@
         Dzieląc wielomian stopnia $n \geq 0$ przez wielomian stopnia $m \geq 0$ otrzymamy wielomian stopnia $n - m$ oraz resztę równą $0$ lub będącą wielomianem 
         stopnia mniejszego od $m$.
     </p>
+	</lesson-chapter>
+
+	<lesson-chapter><template #title>Twierdzenie Bézouta</template>
+    <p class = "type">
+		Formalnie
+	</p>
+
+    <p> 
+        Całe to szaleństwo równań wielomianowych sprowadza się do szukania rozwiązań przez odpowiedni rozkład wielomianu na czynniki. Ale skąd mamy pewność, 
+        że rozkład ten jest w ogóle możliwy? Mówi o tym fakt znany jako "twierdzenie Bézouta", mimo że francuski matematyk Étienne Bézout (<comment 
+        text="Jak widać, poniższe twierdzenie rzeczywiście nie zawiera 'u'">czyt. "bezu"</comment>) nie jest jego autorem. 
+        Poza polską fakt ten nosi nazwę "małe twierdzenie Bézouta" lub "factor theorem" - twierdzenie o rozkładzie.
+    </p>
+
+    <p class="formula">
+		Wielomian zmiennej $x$ osiąga wartość $0$ dla $x = a$ wtedy i tylko wtedy, gdy jest podzielny przez wielomian $x - a$.
+	</p>
+	<div class="proof">
+		<div class = "proof-sticker">Uzasadnienie</div>
+        <p>
+            Twiedzenie to mówi, że zdanie "$a$ jest rozwiązaniem równania wielomianowego" można wyrazić równoważnie zdaniem "$(x - a)$ jest jednym z czynników 
+            tego wielomianu". Nie powinno dziwić, że jeśli $(x - a)$ pojawia się jako czynnik, to w równaniu $(x - a)(\dots \dots)(\dots \dots) = 0$ 
+            jednym z rozwiązań tego równania jest $x = a$. Mniej oczywiste jest, że działa to też na odwrót: jesli $x = a$ rozwiązuje równanie, to 
+            na pewno wielomian da się rozłożyć na czynniki, wśród których znajdzie się $(x - a)$. 
+        </p>
+
+        <p>
+            Wynika to z procedury dzielenia wielomianów w słupku. Przypuśćmy że obliczyliśmy $x = a$ jako rozwiązanie wielomianu i przystąpiliśmy do dzielenia pisemnego 
+            przez $(x - a)$, w rezultacie otrzymując resztę $r$. Według spostrzeżenia dokonanego w poprzedniej sekcji, w wyniku dzielenia mogliśmy otrzymać 
+            jedynie resztę $0$ lub resztę będącą wielomianem stopnia niższego niż $1$, czyli stopnia zerowego; innymi słowy resztą z dzielenia przez $x - a$ może być tylko 
+            liczba. W wyniku dzielenia mielibyśmy coś w rodzaju: 
+        </p>
+            $$\ldots x^4 + \ldots x^3 + \ldots x^2 + \ldots x + \ldots = (x - a)(\ldots \ldots) + r$$
+        <p>
+            Jednak skoro podstawienie $x = a$ zeruje wielomian po lewej stronie, musi też zerować wyrażenie po prawej, czyli na pewno $r = 0$, a więc dzielenie 
+            musiało odbyć się bez reszty, a $(x - a)$ istotnie jest jednym z czynników naszego wielomianu.
+        </p>
+	</div>
+	<div class="proof">
+		<div class = "proof-sticker">Dowód</div>
+		<p> 
+            Niech $W$ oznacza wielomian zmiennej $x$.
+        </p>
+
+        <p>
+            Jeśli $W$ jest podzielny przez $x - a$, mamy $W = (x - a)P$ dla pewnego wielomianu $P$, a więc dla $x = a$ zachodzi $W = (a - a)P = 0\cdot P = 0$, zatem 
+            $W$ osiąga wartość $0$ dla $x = a$.
+        </p>
+
+        <p> 
+            Przypuśćmy teraz, że $W$ osiąga wartość $0$ dla $x = a$. Mamy $W = (x - a)P + r$ dla pewnego wielomianu $P$ oraz reszty $r$. Reszta ta jest zerem lub 
+            wielomianem o stopniu niższym od stopnia dwumianu $x - a$, czyli ma stopień $0$ (jest liczbą rzeczywistą). Dla $x = a$ mamy więc $0 = W = (a - a)P + r = r$, czyli 
+            $r = 0$, a więc $W$ jest podzielny przez $x - a$. □
+        </p>
+	</div>
+	</lesson-chapter>
+
+	<lesson-chapter><template #title>No to w końcu jak rozwiązywać te równania?</template>
+    <p class = "type">
+		Intuicje
+	</p>
+
+    <p> 
+        Choć zabrzmi to trochę żałośnie - zgadywanie jest wszystkim, co mamy. Ale nie jest to zgadywanie "na ślepo". Będziemy zgadywać inteligentnie, jak wykształceni kadeci 
+        algebry. 
+    </p>
+
+    <p> 
+        Zakładamy, że rozwiązania wielomianu dają się przedstawić w postaci liczb wymiernych $\frac{p_1}{q_1}$, $\frac{p_2}{q_2}$, $\frac{p_3}{q_3}$, ..., $\frac{p_n}{q_n}$, ..., 
+        co odpowiada postaci iloczynowej 
+    </p>
+
+    $$\left(x - \frac{p_1}{q_1}\right)\left(x - \frac{p_2}{q_2}\right)\left(x - \frac{p_3}{q_3}\right) \dots \left(x - \frac{p_n}{q_n}\right)$$
+
+    <p> 
+        Lub równoważnie: 
+    </p>
+
+    $$(q_1x - p_1)(q_2x - p_2)(q_3x - p_3) \dots (q_nx - p_n)$$
+
+    <p> 
+        Po rozpisaniu tej postaci iloczynowej, czyli wymnożeniu wszystkich nawiasów, wyraz wolny powstanie z pomnożenia $p_1$, $p_2$, $p_3$, ..., $p_n$, zaś współczynnik 
+        stojący przy najwyższej potędze $x^n$ powstanie z pomnożenia $q_1$, $q_2$, $q_3$, ..., $q_n$. Zatem: gdy patrzymy na równanie wielomianowe, wiemy, że wyraz 
+        wolny stanowi iloczyn liczników rozwiązań, zaś współczynnik przy $x^n$ stanowi iloczyn mianowników rozwiązań.
+    </p>
+
+    <p> 
+        Podsumowując wiedzę zebraną w powyższych rozdziałach, nasza metoda na rozwiązywanie równań wielomianowych wygląda następująco:
+    </p>
+
+    <ul>
+        <li>Patrzymy na współczynniki wielomianu: wyraz wolny $a_0$ oraz współczynnik $a_n$ przy najwyższej potędze $x^n$. Wypisujemy wszystkie dzielniki 
+            naturalne $a_0$ oraz $a_n$. Możliwe rozwiązania równania to ułamki $\pm \frac{p}{q}$, których licznik $p$ jest dzielnikiem $a_0$, a mianownik 
+            $q$ dzielnikiem $a_n$.
+        </li>
+        <li>Przeglądamy możliwe rozwiązania i podstawiamy je pod $x$. Gdy okaże się, że dla $x = \frac{p}{q}$ wielomian się zeruje, mamy jedno z rozwiązań.</li>
+        <li>Zgodnie z twierdzeniem Bézouta wielomian jest na pewno podzielny przez $x - \frac{p}{q}$. Będziemy chcieli go rozłożyć na czynniki.</li>
+        <li>Wykonujemy dzielenie pisemne wielomianu przez dwumian $x - \frac{p}{q}$ lub, równoważnie, przez $qx - p$; powinniśmy otrzymać resztę $0$. Przedstawiamy wielomian w postaci iloczynu.</li>
+        <li>Zajmujemy się nowo otrzymanym wielomianem o niższym stopniu. Powtarzamy całą procedurę zgadywania i kontynuujemy rozkład wielomianu. 
+            Jeżeli otrzymamy wielomian drugiego stopnia, możemy skorzystać ze wzoru na deltę, by dokończyć rozkład.</li>
+    </ul>
+
+    <p> 
+        Należy mieć na uwadze, że przedstawiona metoda pozwala znaleźć <b>tylko</b> rozwiązania wymierne. Nie znajdziemy z jej pomocą np. rozwiązań 
+        zawierających pierwiastki. Działa ona tylko pod warunkiem, że wszystkie współczynniki wielomianu są liczbami całkowitymi. Nie są znane lepsze i bardziej uniwersalne 
+        metody rozwiązywania równań wielomianowych. Dla równań trzeciego i czwartego stopnia istnieją skomplikowane "wzory Cardano" podobne do wzorów z deltą. Dla równań 
+        o stopniu wyższym niż czwarty nie istnieją żadne metody na znajdowanie rozwiązań niewymiernych.
+    </p>
+
+    <div class="example">
+        $$x^3 - 12x + 16 = 0$$
+        <p> 
+            Wyrazem wolnym jest liczba $16$ mająca dzielniki $\{1, 2, 4, 8, 16\}$. Przy najwyższej potędze stoi współczynnik $1$. W takim razie możliwe rozwiązania 
+            wymierne tego równania to $1, 2, 4, 8, 16, -1, -2, -4, -8, -16$. Bierzemy kolejne wartości i sprawdzamy.
+        </p>
+
+        <p> 
+            Dla $x = 1$ mamy $1^3 - 12 \cdot 1 + 16 = 5 \neq 0$. Pudło.<br>
+            Dla $x = 2$ mamy $2^3 - 12 \cdot 2 + 16 = 8 - 24 + 16 = 0$. Znaleźliśmy rozwiązanie $x = 2$, przystępujemy do dzielenia wielomianu przez $x - 2$.
+        </p>
+
+        <columnar-operation-table :operation='""' :numbers='[["/u:\\ \\ x^2", "/u:+2x", "/u:-8", "/u:", "/u:", "/u:", "/u:"], 
+            ["\\ \\ x^3", "+0x^2", "-12x", "+16", ":", "x", "-2"], ["/u:-x^3", "/u:+2x^2", "/u:", "/u:", "/u:", "/u:", "/u:"],
+            ["", "\\ \\ 2x^2", "-12x", "+16", "", "", ""], ["/u:", "/u:-2x^2", "/u:+4x", "/u:", "/u:", "/u:", "/u:"],
+            ["", "", "-8x", "+16", "", "", ""], ["/u:", "/u:", "/u:\\ \\ 8x", "/u:-16", "/u:", "/u:", "/u:"],
+            ["", "", "", "\\ \\ 0", "", "", ""]]'></columnar-operation-table>
+
+        <p> 
+            Równanie upraszcza się do postaci
+        </p>
+
+        $$
+            (x - 2)(x^2 + 2x - 8) = 0
+        $$
+
+        <p> 
+            Równanie kwadratowe rozwiązujemy przy pomocy wzoru na deltę: $\Delta = b^2 - 4ac = 2^2 - 4\cdot 1 \cdot (-8) = 36$, czyli $\sqrt{\Delta} = 6$, 
+            co daje dwa rozwiązania $x = \frac{-2 + 6}{2} = 2$ oraz $x = \frac{-2 - 6}{2} = -4$. Ostatecznie: rozwiązaniami równania są $2$ oraz $4$. 
+            Rozkład wielomianu na czynniki kończy iloczyn:
+        </p>
+
+        $$
+            (x - 2)^2(x + 4) = 0
+        $$
+
+        <p> 
+            Rozwiązanie $x = 2$ pojawiło się "dwa razy", przez co nazywamy je <i>rozwiązaniem podwójnym</i> (lub <i>dwukrotnym</i>).
+        </p>
+    </div>
+
+    <div class="example"> 
+        $$
+            2x^4+7x^3−6x^2−23x+12 = 0
+        $$
+        
+        <p> 
+            Wyraz wolny $12$ ma dzielniki $1, 2, 3, 4, 6, 12$, a współczynnik przy $x^4$ ma dzielniki $1$ i $2$. Możliwe rozwiązania to $1, 2, 3, 4, 6, 12, 
+            \frac{1}{2}, \frac{3}{2}, -1, -2, -3, -4, -6, -12, -\frac{1}{2}, -\frac{3}{2}$. 
+        </p>
+
+        <p> 
+            Dla $x = 1$ mamy $2\cdot 1^4 + 7\cdot 1^3 - 6\cdot 1^2 - 23\cdot 1 + 12 = -8 \lt 0$.<br> 
+            Dla $x = 2$ mamy $2\cdot 2^4 + 7\cdot 2^3 - 6\cdot 2^2 - 23\cdot 2 + 12 = 32 + 56 - 24 - 56 + 12 = 20 \gt 0$.<br>
+            Dla $x = 3$ mamy $2\cdot 3^4 + 7\cdot 3^3 - 6\cdot 3^2 - 23\cdot 3 + 12 = 162 + 63 - 54 - 69 + 12 = 114 \gt 0$.<br>
+            Wartość $x^4$ bardzo szybko rośnie dla coraz większych $x$; mogło by się wydawać, że dla $x$ jeszcze większych niż $3$ wielomian 
+            będzie coraz bardziej oddalał się od $0$. Spróbujmy z mniejszymi wartościami.<br>
+            Dla $x = \frac{1}{2}$ mamy $2\cdot \left(\frac{1}{2}\right)^4 + 7\cdot \left(\frac{1}{2}\right)^3 - 6\cdot \left(\frac{1}{2}\right)^2 - 23\cdot \frac{1}{2} + 12 = 
+            \frac{1}{8} + \frac{7}{8} - \frac{3}{2} - \frac{23}{2} + 12 = 0$. Znaleźliśmy rozwiązanie $x = \frac{1}{2}$, przystępujemy do dzielenia pisemnego przez 
+            $2x - 1$.<br>
+        </p>
+
+        <columnar-operation-table :operation='""' :numbers='[["/u:\\ \\ x^3", "/u:+4x^2", "/u:-x", "/u:-12", "/u:", "/u:", "/u:", "/u:"], 
+            ["\\ \\ 2x^4", "+7x^3", "-6x^2", "-23x", "+12", ":", "2x", "-1"], ["/u:-2x^4", "/u:+x^3", "/u:-8", "/u:", "/u:", "/u:", "/u:", "/u:"],
+            ["", "\\ \\ 8x^3", "-6x^2", "-23x", "+12", "", "", ""], ["/u:", "/u:-8x^3", "/u:+4x^2", "/u:", "/u:", "/u:", "/u:", "/u:"],
+            ["", "", "-2x^2", "-23x", "+12", "", "", ""], ["/u:", "/u:", "/u:\\ \\ 2x^2", "/u:-x", "/u:", "/u:", "/u:", "/u:"], 
+            ["", "", "", "-24x", "+12", "", "", ""], ["/u:", "/u:", "/u:", "/u:\\ \\ 24x", "/u:-12", "/u:", "/u:", "/u:"],
+            ["", "", "", "", "\\ \\ 0", "", "", ""]]'>
+        </columnar-operation-table>
+        
+        <p>
+            Równanie upraszcza się do iloczynu  
+        </p>
+
+        $$
+            (2x - 1)(x^3 + 4x^2 - x - 12) = 0
+        $$
+
+        <p> 
+            Zajmijmy się równaniem sześciennym $x^3 + 4x^2 - x - 12 = 0$. Wyraz wolny ma dzielniki $1, 2, 3, 4, 6, 12$, a współczynnik przy $x^3$ dzieli się 
+            tylko przez $1$. Możliwe rozwiązania wymierne zawężają się do $1, 2, 3, 4, 6, 12, -1, -2, -3, -4, -6, -12$. Wykluczyliśmy już rozwiązania $x = 1$, 
+            $x = 2$, $x = 3$ i niechętnie spojrzeliśmy na rozwiązania o wyższych wartościach. Spróbujmy z ujemnymi.
+        </p>
+
+        <p> 
+            Dla $x = -1$ mamy $(-1)^3 + 4(-1)^2 - (-1) - 12 = -1 + 4 + 1 - 12 = -8 \lt 0$<br>
+            Dla $x = -2$ mamy $(-2)^3 + 4(-2)^2 - (-2) - 12 = -8 + 16 + 2 - 12 = -2 \lt 0$<br>
+            Dla $x = -3$ mamy $(-3)^3 + 4(-3)^2 - (-3) - 12 = -27 + 36 + 3 - 12 = 0 \lt 0$. Mając kolejne rozwiązanie $x = -3$, przystępujemy do dzielenia przez 
+            $x + 3$.
+        </p>
+
+        <columnar-operation-table :operation='""' :numbers='[["/u:\\ \\ x^2", "/u:+x", "/u:-4", "/u:", "/u:", "/u:", "/u:"], 
+            ["\\ \\ x^3", "+4x^2", "-x", "-12", ":", "x", "+3"], ["/u:-x^3", "/u:-3x^2", "/u:", "/u:", "/u:", "/u:", "/u:"],
+            ["", "\\ \\ x^2", "-x", "-12", "", "", ""], ["/u:", "/u:-x^2", "/u:-3x", "/u:", "/u:", "/u:", "/u:"],
+            ["", "", "-4x", "-12", "", "", ""], ["/u:", "/u:", "/u:\\ \\ 4x", "/u:+12", "/u:", "/u:", "/u:"],
+            ["", "", "", "\\ \\ 0", "", "", ""]]'>
+        </columnar-operation-table>
+
+        <p> 
+            Rozkład postępuje do postaci
+        </p>
+
+        $$
+            (2x - 1)(x + 3)(x^2 + x - 4) = 0
+        $$
+
+        <p> 
+            Zostało nam równanie kwadratowe, które rozpracowujemy deltą: $\Delta = 1^2 - 4 \cdot 1 \cdot (-4) = 17 \Rightarrow \sqrt{\Delta} = \sqrt{17}$, skąd dwa 
+            rozwiązania $\frac{-1-\sqrt{17}}{2}$ oraz $\frac{-1+\sqrt{17}}{2}$. Tych rozwiązań nie mieliśmy szans odgadnąć podczas rozkładu wielomianu - delta była jedynym 
+            wyjściem.
+        </p>
+
+        <p> 
+            Ostatecznie mamy cztery rozwiązania: $\left\{\frac{1}{2}, -3, -\frac{1 + \sqrt{17}}{2}, \frac{-1+\sqrt{17}}{2}\right\}$, a postać iloczynowa przedstawia się jako:
+        </p>
+
+        $$
+            (2x - 1)(x + 3)\left(x + \frac{1 + \sqrt{17}}{2}\right)\left(x - \frac{-1+\sqrt{17}}{2}\right) = 0
+        $$
+
+    </div>
 	</lesson-chapter>
 
   </lesson>
