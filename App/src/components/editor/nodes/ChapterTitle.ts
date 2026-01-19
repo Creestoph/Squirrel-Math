bc28d4672603387aefcdc6a5badcb32c0cd92383
Bober RS <boberrs@gmail.com>
2026-01-19 21:37:46 +0100

    fixed inserting content above chapter title

--- App/src/components/editor/nodes/ChapterTitle.ts
+++ App/src/components/editor/nodes/ChapterTitle.ts
@@ -3,8 +3,9 @@ import { Editor } from '@tiptap/core';
 import { VueNodeViewRenderer } from '@tiptap/vue-3';
 import ChapterTitleVue from './ChapterTitle.vue';
 import { GapCursor } from 'prosemirror-gapcursor';
-import { Plugin } from '@tiptap/pm/state';
+import { EditorState, Plugin } from '@tiptap/pm/state';
 import { dropNewlines } from '../tiptap-utils';
+import { Node as PMNode } from '@tiptap/pm/model';
 
 export default Node.create({
     name: 'chapterTitle',
@@ -22,13 +23,25 @@ export default Node.create({
     }),
 
     addKeyboardShortcuts() {
+        const shouldPlaceGapCursor = (node: PMNode | null) => {
+            return node?.isBlock && (!node.isTextblock || node.type.name === 'customElement');
+        };
+
+        const getPositionBeforeChapter = (state: EditorState) => {
+            const { $from } = state.selection;
+            const chapterDepth = $from.depth - 1;
+            const chapterPos = $from.before(chapterDepth);
+            const $boundary = state.doc.resolve(chapterPos);
+            const nodeBeforeChapter = $boundary.nodeBefore!;
+            const isIntroAbove = nodeBeforeChapter.type.name === 'intro';
+            return isIntroAbove ? chapterPos - 1 : chapterPos - 2;
+        };
+
         /**
          * Needed to support arrow navigation from chapter title; for some reason, it doesn't work by default.
          */
         const moveToChapterBody = ({ editor }: { editor: Editor }) => {
             const { $from } = editor.state.selection;
-
-            // Only handle if we're in chapter title
             if ($from.parent.type.name !== this.name) {
                 return false;
             }
@@ -45,16 +58,49 @@ export default Node.create({
 
             const $targetPos = editor.state.doc.resolve(targetPos - 1);
             const nodeAfter = $targetPos.nodeAfter;
-            if (nodeAfter?.isBlock && (!nodeAfter.isTextblock || nodeAfter.type.name === 'customElement')) {
+            if (shouldPlaceGapCursor(nodeAfter)) {
                 editor.view.dispatch(editor.state.tr.setSelection(new GapCursor($targetPos)));
                 return true;
             }
 
             return editor.commands.setTextSelection(targetPos);
         };
+
+        const moveAboveChapterTitle = ({ editor }: { editor: Editor }) => {
+            const { state, view } = editor;
+            const { $from } = state.selection;
+            if ($from.parent.type.name !== this.name) {
+                return false;
+            }
+
+            const posBeforeChapter$ = state.doc.resolve(getPositionBeforeChapter(state));
+            const nodeBeforeChapter = posBeforeChapter$.nodeBefore;
+
+            if (shouldPlaceGapCursor(nodeBeforeChapter)) {
+                view.dispatch(state.tr.setSelection(new GapCursor(posBeforeChapter$)));
+                return true;
+            }
+
+            return false;
+        };
+
+        const enterAtStartCreatesParagraphAbove = ({ editor }: { editor: Editor }) => {
+            const { state, view } = editor;
+            const { $from } = state.selection;
+            if ($from.parent.type.name !== this.name || $from.parentOffset !== 0) {
+                return false;
+            }
+
+            const tr = state.tr.insert(getPositionBeforeChapter(state), state.schema.nodes.paragraph.createAndFill()!);
+            view.dispatch(tr.scrollIntoView());
+
+            return true;
+        };
+
         return {
             ArrowDown: moveToChapterBody,
-            Enter: moveToChapterBody,
+            ArrowUp: moveAboveChapterTitle,
+            Enter: enterAtStartCreatesParagraphAbove,
         };
     },
 
