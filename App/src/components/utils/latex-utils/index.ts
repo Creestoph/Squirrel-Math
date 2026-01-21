5486e3d7b35d1aa99c5d922bbb7ac9b8f4ac2837
Krzysztof Strzelecki <krzysztof.strzelecki@output42.com>
2026-01-21 13:24:47 +0100

    Develop more universal way to display math (MathJax) #63 - use functions copied from Katex

--- App/src/components/utils/latex-utils/index.ts
+++ App/src/components/utils/latex-utils/index.ts
@@ -1,4 +1,5 @@
 import temml from 'temml';
+import renderMathInElement from './renderMathInElement';
 
 export interface LatexRenderer {
     recalculateWholePage(): void;
@@ -7,35 +8,10 @@ export interface LatexRenderer {
 
 export const useLatexRenderer: () => LatexRenderer = () => ({
     recalculateWholePage: () => {
-        const walker = document.createTreeWalker(
-            document.body,
-            NodeFilter.SHOW_TEXT
-        );
-        
-        const nodes: Node[] = [];
-        let node = walker.nextNode();
-        while (node) {
-            if (node.textContent?.match(/\$\$[\s\S]+?\$\$|\$[^$]+?\$/)) {
-                nodes.push(node);
-            }
-            node = walker.nextNode();
-        }
-        
-        nodes.forEach(n => {
-            const parent = n.parentElement;
-            if (!parent) {
-                return;
-            }
-
-            const text = n.textContent || '';
-            const html = text
-                .replace(/\$\$([\s\S]+?)\$\$/g, (_, latex) => temml.renderToString(latex, {displayMode: true}))
-                .replace(/\$([^$]+?)\$/g, (_, latex) => temml.renderToString(latex, {displayMode: false}));
-            
-            const span = document.createElement('span');
-            span.innerHTML = html;
-            parent.replaceChild(span, n);
-        });
+        renderMathInElement(document.body, { delimeters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+        ]});
     },
     render: (latex: string, displayMode: boolean = false): string => {
         return temml.renderToString(latex, { displayMode });
