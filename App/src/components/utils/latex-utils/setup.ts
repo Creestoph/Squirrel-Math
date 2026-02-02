e8575bc7f912f2a2238152d1c6735d77c547d9d0
Krzysztof Strzelecki <krzysztof.strzelecki@output42.com>
2026-02-02 21:25:30 +0100

    Develop more universal way to display math (MathJax) #63 - 🙄

--- App/src/components/utils/latex-utils/setup.ts
+++ App/src/components/utils/latex-utils/setup.ts
@@ -1,7 +1,7 @@
 import 'katex/dist/katex.min.css';
 
-export const MATH_FONT_CSS_VAR = '--math-font-name';
-export const DEFAULT_MATH_FONT = 'KaTeX_Main';
+const MATH_FONT_CSS_VAR = '--math-font-name';
+const DEFAULT_MATH_FONT = 'KaTeX_Main';
 
 // Set CSS variable
 document.documentElement.style.setProperty(MATH_FONT_CSS_VAR, DEFAULT_MATH_FONT);
