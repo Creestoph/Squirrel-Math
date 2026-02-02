5ce089a838e8b255c82e1fd5b3d7035eba1d5d1d
Krzysztof Strzelecki <krzysztof.strzelecki@output42.com>
2026-02-02 21:31:12 +0100

    Develop more universal way to display math (MathJax) #63 - 🙄

--- App/src/components/builtin-components/arithmetic-calculator/ArithmeticCalculator.vue
+++ App/src/components/builtin-components/arithmetic-calculator/ArithmeticCalculator.vue
@@ -4,13 +4,16 @@
             <p class="math no-selection">
                 <latex-render :latex="stepData.mathJax ?? ''" :displayMode="!!stepData.mathJax" />
             </p>
-            <p class="comment no-selection" v-html="stepData.comment"></p>
+            <p class="comment no-selection">
+                <latex-text :text="stepData.comment ?? ''" />
+            </p>
         </template>
     </step-by-step>
 </template>
 
 <script setup lang="ts">
 import LatexRender from '@/components/utils/latex-utils/LatexRender.vue';
+import LatexText from '@/components/utils/latex-utils/LatexText.vue';
 import { ArithmeticCalculatorLogic } from './arithmetic-calculator-logic';
 import StepByStep from '@/components/utils/StepByStep.vue';
 
